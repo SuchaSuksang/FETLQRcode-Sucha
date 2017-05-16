177640df29c9f29e40d8deab4d1a41033cb35162
traineeSucha <sucha.s@th.fujikura.com>
2017-05-16 16:07:45 +0700

    Sucha Modify 16-May-2017

--- app/src/main/java/appewtc/masterung/fetlqrcode/RegisterActivity.java
+++ app/src/main/java/appewtc/masterung/fetlqrcode/RegisterActivity.java
@@ -2,12 +2,80 @@ package appewtc.masterung.fetlqrcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Exclicit
+    private EditText nameEditText, userEditText, passwordEditText;
+
+    private ImageView imageView;
+    private Button button;
+    private String nameString,userString,passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initialView();
+
+//Controller
+        controller();
+    }  // Main Method
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
     }
-}
+
+
+    private void controller() {
+        imageView.setOnClickListener(this);
+        button.setOnClickListener(this);
+    }
+
+    private void initialView() {
+        nameEditText = (EditText) findViewById(R.id.edtName);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText= (EditText) findViewById(R.id.edtPassword);
+        imageView = (ImageView) findViewById(R.id.btnBack);
+        button = (Button) findViewById(R.id.btnNewRegis);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For Back
+        if (v == imageView){
+            finish();;
+        }
+
+        //For Button
+        if(v == button){
+            //get value from editText
+            nameString = nameEditText.getText().toString().trim();
+            userString = userEditText.getText().toString().trim();
+            passwordString = passwordEditText.getText().toString().trim();
+
+            //Check Space
+            if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+                //Have Space
+                MyAlert myAlert = new MyAlert(this);
+                myAlert.myDialog(getResources().getString(R.string.title_HaveSpace),getResources().getString(R.string.message_HaveSpace));
+
+
+            } else {
+                //No Space
+
+
+            }
+
+        }
+
+    }
+} //Main Class
